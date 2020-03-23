f168384a1a6c0aef4b84cc7888df3802eec0ac87
Grey <sergejyurskyj@yahoo.com>
2020-03-22 20:48:27 -0700

    Committed 2.4. Scalars, Vectors, and Matrices

--- Linear Algebra for Data Science in Python/2.4. Scalars, Vectors, and Matrices.py
+++ Linear Algebra for Data Science in Python/2.4. Scalars, Vectors, and Matrices.py
@@ -8,55 +8,41 @@ import numpy as np
 s = 5
 
 print(s)
+print('#' * 50, 'Vectors')
 
 # Vectors
-
 v = np.array([5,-2,4])
-
 print(v)
 
 # Matrices
+print('#' * 50, 'Matrices')
 
 m = np.array([[5,12,6],[-3,0,14]])
-
 print(m)
 
 # Data types
-
+print('#' * 50, 'Data types')
 print(type(s))
-
 print(type(v))
-
 print(type(m))
-
 s_array = np.array(5)
-
 print(type(s_array))
 
 # Data shapes
-
-# m.shape
-#
-# v.shape
-#
+print('#' * 50, 'Data shapes')
+print(m.shape)
+print(v.shape)
 # s.shape
-#
-# s_array.shape
+print(s_array.shape)
 
 # Creating a column vector
+print('#' * 50, 'Creating a column vector')
 
-v.reshape(1,3)
-
+print(v.reshape(1,3))
 v.reshape(3,1)
-
 print(m+s)
-
 print(m+s_array)
-
 print(v+s)
-
 print(v)
-
 print(v+s_array)
-
 print(m+v)
