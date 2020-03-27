a1ca5b92e954d9496ab69ebcbeed8bca3caa8f9f
Grey <sergejyurskyj@yahoo.com>
2020-03-26 20:37:46 -0700

    Committed 2.5. Tensors

--- Linear Algebra for Data Science in Python/2.5. Tensors.py
+++ Linear Algebra for Data Science in Python/2.5. Tensors.py
@@ -1,27 +1,56 @@
-# Import the relevant libraries
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# # Import the relevant libraries
 
+# %%
 import numpy as np
 
-# Creating a tensor
+# %% [markdown]
+# ## Creating a tensor
 
+# %%
 m1 = np.array([[5,12,6],[-3,0,14]])
-
 m1
 
-m2 = np.array([[9,8,7],[1,3,-5]])
 
+# %%
+m2 = np.array([[9,8,7],[1,3,-5]])
 m2
 
+
+# %%
 t = np.array([m1,m2])
 
+
+# %%
 t
 
-# Checking its shape
+# %% [markdown]
+# ## Checking its shape
 
+# %%
 t.shape
 
-# Manually creating a tensor
+# %% [markdown]
+# ## Manually creating a tensor
 
+# %%
 t_manual = np.array([[[ 5, 12,  6], [-3,  0, 14]], [[ 9,  8,  7], [ 1,  3, -5]]])
 
+
+# %%
 t_manual
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
