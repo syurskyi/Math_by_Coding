0a15189551180057d1552a465ae0685e62a35541
Grey <sergejyurskyj@yahoo.com>
2020-03-26 20:39:20 -0700

    Committed 2.9. Dot product

---
+++ Linear Algebra for Data Science in Python/2.9. Dot product.py
@@ -0,0 +1,48 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# ## Import relevant libraries
+
+# %%
+import numpy as np
+
+# %% [markdown]
+# ## Dot product
+
+# %%
+x = np.array([2,8,-4])
+y = np.array([1,-7,3])
+
+
+# %%
+np.dot(x,y)
+
+
+# %%
+u = np.array([0,2,5,8])
+v = np.array([20,3,4,-1])
+
+
+# %%
+np.dot(u,v)
+
+# %% [markdown]
+# ## Scalar * Scalar
+
+# %%
+np.dot(5,6)
+
+
+# %%
+np.dot(10,-2)
+
+# %% [markdown]
+# ## Scalar * Vector
+
+# %%
+x
+
+
+# %%
+5*x
+
